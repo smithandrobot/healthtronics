f0f8ffd23459b185608922c3ad5b5e9510a21b3a
David <david@smithandrobot.com>
2012-04-30 18:03:44 -0500

    updated

--- themes/templates/node--resource_library_page.tpl.php
+++ themes/templates/node--resource_library_page.tpl.php
@@ -4,7 +4,7 @@
 	print_r($node->title);
 	$view = views_get_view('lithotripsy_resources');
 	//output the top three items in the view with the node title as an argument
-	print views_embed_view('lithotripsy_resources', 'page');
+	print views_embed_view('lithotripsy_resources', 'block');
 	print '<br />End node template<br />';
 	// access the view
 ?>
