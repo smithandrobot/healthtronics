626b0d6fb4eb63feda56f589bc6581da88637c5e
David <david@smithandrobot.com>
2012-05-26 13:55:06 -0500

    resources bkg added to tpl file rendering

--- themes/templates/node--resource.tpl.php
+++ themes/templates/node--resource.tpl.php
@@ -5,7 +5,9 @@
 	$file = '/sites/default/files/' . file_uri_target($node->field_file_reference['und'][0]['uri']);
 	$resource = strtolower($resource_type->name);
 	$video_link = $node->field_video_embed['und'][0]['value'];
-
+	$image = $node->field_resource_thumbnail['und'][0]['uri'];
+	if(isset($image)) $image = '/sites/default/files/' . file_uri_target($image);
+	
 	switch($resource)
 	{
 		case 'video':
@@ -26,23 +28,10 @@
 			$color 	='mol-border-green';
 			break;
 	}
-	// $color
-	/*
-field_summary
-field_resource_type
-field_file_reference
-field_resource_thumbnail
-field_video_embed
-field_site_categories
-Video = molecule_icon_01.png - mol-border-purple icon-01
-File Download = molecule_icon_03.png - mol-border-green icon-03
-Offsite Link = molecule_icon_02.png - mol-border-blue
-Image = molecule_icon_04.png - mol-border-grey
-
-
-	*/
 ?>
-<?php if($teaser): ?>
+
+<!-- No Image BKG -->
+<?php if($teaser && !isset($image)): ?>
 <div class="span3 small-mol-resize-d">
 	<?php if($resource == 'video'): ?>
 	<a class="video_open" data-toggle="modal" data-video="<?php print $video_link; ?>" href="<?php print $file; ?>">
@@ -54,6 +43,24 @@ Image = molecule_icon_04.png - mol-border-grey
 				<h1><?php print $node->title; ?></h1>
 				<p><?php print $summary; ?></p>
 				<div class="icon <?php print $icon; ?>"></div>
+			</div>
+		</div>
+	</a>
+</div>
+<?php endif?>
+
+<?php if($teaser && isset($image)): ?>
+<div class="span3 small-mol-resize-d">
+	<?php if($resource == 'video'): ?>
+	<a class="video_open" data-toggle="modal" data-video="<?php print $video_link; ?>" href="<?php print $file; ?>">
+	<?php else: ?>
+	<a href="<?php print $file; ?>">
+	<?php endif; ?>
+		<div style="background-image:url(<?php print $image; ?>);" 
+			class="video-molecule <?php print $color; ?>">
+			<div class="video-molecule-description">
+				<p><?php print $summary; ?></p>
+				<div class="video-molecule-content-background"></div>
 			</div>
 		</div>
 	</a>
