69749b737421ea6acf92c235b02b0a3fafc15e72
David <david@smithandrobot.com>
2012-05-26 08:46:21 -0500

    added margin to top of contact form

--- themes/template.php
+++ themes/template.php
@@ -4,18 +4,70 @@
  * Automatically rebuild the theme registry.
  * Uncomment to use during development.
  */
+
+/**
+ * OG TAGS
+ * og:title
+ * og:type
+ * og:image
+ * og:url
+ * og:description
+ */
 drupal_theme_rebuild();
 
-function healthtronicsv2_preprocess_node(&$vars)
+function healthtronicsv2_preprocess_page(&$vars)
 {
-	$element = array(
-	    '#tag' => 'meta',
-	    '#attributes' => array(
-	        'property' => 'keywords',
-	        'content' => 'Urology',
-	    ),
-	);
-
-	drupal_add_html_head($element, 'meta_keywords');
+    $node = $vars['node'];
+	$image = url('/sites/all/themes/healthtronicsv2/images/logo_ht.png', array('absolute'=>TRUE));
+	print($vars['view_mode']);
+	if(isset($node) && $vars['view_mode']=='full')
+	{
+	   	// var_dump($node);
+	   	$url = url('node/'.$node->nid, array('absolute'=>TRUE));
+	   	$summary = strip_tags($node->body['und'][0]['safe_summary']);
+	   	$title = strip_tags($node->title);
+	   	
+	   	$elements = array(
+	   	       			'meta_desc' => array(
+	   	       			    '#tag' => 'meta',
+	   	       			    '#attributes' => array(
+	   	       			        'name' => 'description',
+	   	       			        'content' => $summary,
+	   	       			    ),
+	   	       			),
+	   	       			'og_summary' => array(
+	   	       			    '#tag' => 'meta',
+	   	       			    '#attributes' => array(
+	   	       			        'property' => 'og:description',
+	   	       			        'content' => $summary,
+	   	       			    ),
+	   	       			),
+	   	       			'og_url' => array(
+	   	       			    '#tag' => 'meta',
+	   	       			    '#attributes' => array(
+	   	       			        'property' => 'og:url',
+	   	       			        'content' => $url,
+	   	       			    ),
+	   	       			),
+	   	       			'og_image' => array(
+	   	       			    '#tag' => 'meta',
+	   	       			    '#attributes' => array(
+	   	       			        'property' => 'og:image',
+	   	       			        'content' => $image,
+	   	       			    ),
+	   	       			),
+	   	       			'og_title' => array(
+	   	       			    '#tag' => 'meta',
+	   	       			    '#attributes' => array(
+	   	       			        'property' => 'og:title',
+	   	       			        'content' => $title,
+	   	       			    ),
+	   	       			),
+	   				);
+	   				
+	   	foreach ($elements as $k => $v) {
+	   	    drupal_add_html_head($v, $k);
+	   	}
+	}
 }
 ?>
