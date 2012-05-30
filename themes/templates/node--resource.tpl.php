4f1f7406cc4fdbc9dfe12a9b63148207c6d7dc1e
Junie Threatt <juniethreatt@gmail.com>
2012-05-30 13:56:55 -0500

    changing resource small-mol-resize-d to C to work in the sidebar. will probably change search result formatting though

--- themes/templates/node--resource.tpl.php
+++ themes/templates/node--resource.tpl.php
@@ -30,9 +30,9 @@
 	}
 ?>
 
-<!-- No Image BKG -->
 <?php if($teaser && !isset($image)): ?>
-<div class="span3 small-mol-resize-d">
+<!-- No Image BKG -->
+<div class="span3 small-mol-resize-c">
 	<?php if($resource == 'video'): ?>
 	<a class="video_open" data-toggle="modal" data-title="<?php print $node->title; ?>" data-summary="<?php print $summary; ?>" data-video="<?php print $video_link; ?>" href="<?php print $file; ?>">
 	<?php else: ?>
@@ -49,13 +49,10 @@
 </div>
 <?php endif?>
 
+
 <?php if($teaser && isset($image)): ?>
 <div class="span3 small-mol-resize-d">
-	<?php if($resource == 'video'): ?>
-	<a class="video_open" data-toggle="modal" data-video="<?php print $video_link; ?>" href="<?php print $file; ?>">
-	<?php else: ?>
 	<a href="<?php print $file; ?>">
-	<?php endif; ?>
 		<div style="background-image:url(<?php print $image; ?>);" class="video-molecule <?php print $color; ?>">
 			<div class="video-molecule-description">
 				<p><?php print $summary; ?></p>
