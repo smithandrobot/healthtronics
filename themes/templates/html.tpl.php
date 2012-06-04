bfb653485d8aa7dfd044e27b2ae9d92872b69ab3
Junie Threatt <juniethreatt@gmail.com>
2012-06-04 11:03:29 -0500

    changed viewport meta data in html.tpl

--- themes/templates/html.tpl.php
+++ themes/templates/html.tpl.php
@@ -49,7 +49,7 @@
 
 	<?php print $head; ?>
 	<title><?php print $head_title; ?></title>
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=0">
 	<?php print $styles; ?>
 	<?php print $scripts; ?>
 
